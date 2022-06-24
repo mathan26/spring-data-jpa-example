8115ca562a4215f9f17afb356f349349f774ab22
Mathankumars <mathankumar.s@healthasyst.com>
2022-06-24 10:54:06 +0530

    initial commit

---
+++ src/main/java/com/github/mathan26/spring/data/jpa/tutorial/springdatajpaexample/SpringDataJpaExampleApplication.java
@@ -0,0 +1,13 @@
+package com.github.mathan26.spring.data.jpa.tutorial.springdatajpaexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataJpaExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/mathan26/spring/data/jpa/tutorial/springdatajpaexample/SpringDataJpaExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.mathan26.spring.data.jpa.tutorial.springdatajpaexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
